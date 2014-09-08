d4ab131a67e23793d60f8f140f578e2c8e86813e
Jairo Eduardo Lopez <jelfn@sislacom.com>
2014-09-09 00:03:32 +0900

    Licensing changes
    Small changes to licensing.

--- scenarios/ndn-mobility-random.cc
+++ scenarios/ndn-mobility-random.cc
@@ -1,25 +1,24 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ndn-mobility-random.cc
+ *  Random walk Wifi Mobile scenario for ndnSIM
  *
  * Copyright (c) 2014 Waseda University
  * Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
  *
- * ndn-mobility-random.cc
- *  Random walk Wifi Mobile scenario for ndnSIM
- *
  * Special thanks to University of Washington for initial templates
+ *
+ *  ndn-mobility-random is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  ndn-mobility-random is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with ndn-mobility-random.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Standard C++ modules
